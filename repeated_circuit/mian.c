76a383b00b6af1ca99256c09afcc88f1e5ac5a2a
5727770@stu.kmu.ac.kr <5727770@stu.kmu.ac.kr>
2023-09-14 23:03:22 +0900

    후위 순회 방식 완성

--- repeated_circuit/mian.c
+++ repeated_circuit/mian.c
@@ -8,22 +8,39 @@ typedef struct TreeNode {
 	struct TreeNode* left, * right;
 } TreeNode;
 
+// 순회를 위한 스택 생성
 #define SIZE 100
-int top = -1;
-TreeNode* stack[SIZE];
+int top1 = -1;
+int top2 = -1;
+TreeNode* stack1[SIZE];
+TreeNode* stack2[SIZE];
 
-// push 함수
-void push(TreeNode* p)
+// 첫번째 스택 push 함수
+void push1(TreeNode* p)
 {
-	if (top < SIZE - 1)
-		stack[++top] = p;
+	if (top1 < SIZE - 1)
+		stack1[++top1] = p;
 }
-// pop 함수
-TreeNode* pop()
+// 첫번째 스택 pop 함수
+TreeNode* pop1()
 {
 	TreeNode* p = NULL;
-	if (top >= 0)
-		p = stack[top--];
+	if (top1 >= 0)
+		p = stack1[top1--];
+	return p;
+}
+// 두번째 스택 push함수
+void push2(TreeNode* p)
+{
+	if (top2 < SIZE - 1)
+		stack2[++top2] = p;
+}
+// 두번째 스택 pop 함수
+TreeNode* pop2()
+{
+	TreeNode* p = NULL;
+	if (top2 >= 0)
+		p = stack2[top2--];
 	return p;
 }
 // 전위 순회
@@ -31,10 +48,10 @@ void preorder_iter(TreeNode* root)
 {
 	while (1) {
 		for (; root; root = root->left) {
-			push(root);
-			printf("%d ", root->data);
+			push1(root);                  //root를 push
+			printf("%d ", root->data);   //root를 출력
 		}
-		root = pop();
+		root = pop1();     
 		if (!root) break;
 		root = root->right;
 	}
@@ -44,18 +61,33 @@ void inorder_iter(TreeNode* root)
 {
 	while (1) {
 		for (; root; root = root->left)
-			push(root);
-		root = pop();
+			push1(root);
+		root = pop1();
 		if (!root) break;
 		printf("%d ", root->data);
 		root = root->right;
 	}
 }
 // 후위 순회
+// 후위 순회
 void postorder_iter(TreeNode* root)
 {
+	while (1) {
+		for (; root; root = root->right) {
+			push1(root);
+			push2(root);
+		}
+		root = pop2(); // 두 번째 스택에서 노드를 꺼냅니다.
+		if (!root) break;
+		root = root->left; // 왼쪽 서브트리로 이동합니다.
+	}
 
+	while (top1 >= 0) {
+		root = pop1(); // 첫 번째 스택에서 노드를 꺼내면서 출력합니다.
+		printf("%d ", root->data);
+	}
 }
+
 
 //링크 노드 값입력
 TreeNode n1 = { 4, NULL, NULL };
