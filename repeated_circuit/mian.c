5c7ebe39960620e9bc07c17903dc206daf8e4fa1
5727770@stu.kmu.ac.kr <5727770@stu.kmu.ac.kr>
2023-09-20 14:57:08 +0900

    0919 과제

--- repeated_circuit/mian.c
+++ repeated_circuit/mian.c
@@ -7,13 +7,13 @@ typedef struct TreeNode {
 } TreeNode;
 
 //링크 노드 값입력
-TreeNode n1 = { 2, NULL, NULL };
-TreeNode n2 = { 3, NULL, NULL };
-TreeNode n3 = { '+', &n1, &n2 };
-TreeNode n4 = { 4, NULL, NULL };
-TreeNode n5 = { 5, NULL, NULL };
-TreeNode n6 = { '*', &n4, &n5 };
-TreeNode n7 = { '+', &n3, &n6 };
+TreeNode n1 = { 4, NULL, NULL };
+TreeNode n2 = { 5, NULL, NULL };
+TreeNode n3 = { '*', &n1, &n2 };
+TreeNode n4 = { 3, NULL, NULL };
+TreeNode n5 = { '+', &n4, &n3};
+TreeNode n6 = { 2, NULL, NULL };
+TreeNode n7 = { '+', &n6, &n5 };
 TreeNode n8 = { 6, NULL, NULL };
 TreeNode n9 = { 7, NULL, NULL };
 TreeNode n10 = { '/', &n8, &n9};
@@ -25,25 +25,38 @@ TreeNode* exp = &n13;
 // 수식 계산 함수
 double evaluate(TreeNode* root)
 {
-    if (root == NULL) return 0; 
-    if (root->left == NULL && root->right == NULL) return root->data; 
+    if (root == NULL) return 0;
+    if (root->left == NULL && root->right == NULL) return root->data;
     else {
         double op1 = evaluate(root->left);
         double op2 = evaluate(root->right);
-        printf("%.2lf %c %.2lf\n", op1, (char)root->data, op2);
-        switch ((char)root->data) { //계산
-        case '+': return op1 + op2;
-        case '-': return op1 - op2;
-        case '*': return op1 * op2;
+
+        //계산
+        double result;
+
+        switch ((char)root->data) {
+        case '+':
+            result = op1 + op2;
+            break;
+        case '-':
+            result = op1 - op2;
+            break;
+        case '*':
+            result = op1 * op2;
+            break;
         case '/':
             if (op2 != 0) {
-                return op1 / op2;
+                result = op1 / op2;
             }
             else {
                 printf("나눗셈 에러\n");
-                exit(1); 
+                exit(1);
             }
+            break;
         }
+
+        printf("%.2lf %c %.2lf = %.2lf\n", op1, (char)root->data, op2, result);
+        return result;
     }
     return 0;
 }
