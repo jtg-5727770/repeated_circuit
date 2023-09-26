2f225839ca8b536ed106fa83c93b702479127c06
5727770@stu.kmu.ac.kr <5727770@stu.kmu.ac.kr>
2023-09-26 10:53:54 +0900

    0926_실습

--- repeated_circuit/mian.c
+++ repeated_circuit/mian.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <memory.h>
+#include <string.h>
 
 
 typedef struct TreeNode {
@@ -8,84 +8,91 @@ typedef struct TreeNode {
 	struct TreeNode* left, * right;
 } TreeNode;
 
-#define SIZE 100
-int top = -1;
-TreeNode* stack[SIZE];
-
-// push 함수
-void push(TreeNode* p)
-{
-	if (top < SIZE - 1)
-		stack[++top] = p;
-}
-// pop 함수
-TreeNode* pop()
-{
-	TreeNode* p = NULL;
-	if (top >= 0)
-		p = stack[top--];
-	return p;
-}
-// 전위 순회
-void preorder_iter(TreeNode* root)
-{
-	while (1) {
-		for (; root; root = root->left) {
-			push(root);
-			printf("%d ", root->data);
-		}
-		root = pop();
-		if (!root) break;
-		root = root->right;
-	}
-}
-// 중위 순회
-void inorder_iter(TreeNode* root)
-{
-	while (1) {
-		for (; root; root = root->left)
-			push(root);
-		root = pop();
-		if (!root) break;
-		printf("%d ", root->data);
-		root = root->right;
-	}
-}
-// 후위 순회
-void postorder_iter(TreeNode* root)
-{
-
-}
-
 //링크 노드 값입력
-TreeNode n1 = { 4, NULL, NULL };
-TreeNode n2 = { 5, NULL, NULL };
-TreeNode n3 = { 3, &n1, &n2 };
-TreeNode n4 = { 6, NULL, NULL };
-TreeNode n5 = { 2, &n3, &n4 };
-TreeNode n6 = { 10, NULL, NULL };
-TreeNode n7 = { 11, NULL, NULL };
-TreeNode n8 = { 9, &n6, &n7 };
-TreeNode n9 = { 8, NULL, NULL };
-TreeNode n10 = { 7, &n9, &n8 };
-TreeNode n11 = { 1, &n5, &n10 };
-TreeNode* root = &n11;
+TreeNode n1 = { 25, NULL, NULL };
+TreeNode n2 = { 16, NULL, &n1 };
+TreeNode n3 = { 42, NULL, NULL };
+TreeNode n4 = { 46, &n3, NULL };
+TreeNode n5 = { 55, NULL, NULL };
+TreeNode n6 = { 53, &n4, &n5 };
+TreeNode n7 = { 41, &n2, &n6 };
+TreeNode n8 = { 62, NULL, NULL };
+TreeNode n9 = { 64, NULL, NULL };
+TreeNode n10 = { 63, &n8, &n9 };
+TreeNode n11 = { 70, NULL, NULL };
+TreeNode n12 = { 65, &n10, &n11 };
+TreeNode n13 = { 74, &n12, NULL };
+TreeNode n14 = { 60, &n7, &n13};
+TreeNode* root = &n13;
+
+// 반복적인 탐색 함수
+TreeNode* search(TreeNode* node, int key)
+{
+	while (node != NULL) {
+		if (key == node->data) return node;
+		else if (key < node->data)
+			node = node->left;
+		else
+			node = node->right;
+	}
+	return NULL; 	// 탐색에 실패했을 경우 NULL 반환
+}
 
 void  main()
 {
-	printf("[Linked Tree]\n");
-	
-	printf("1.전위 순회\n");
-	preorder_iter(root);
-	printf("\n\n");
-	
-	printf("2.중위 순회\n");
-	inorder_iter(root);
-	printf("\n\n");
+	int k;
 
-	printf("3.후위 순회\n");
-	postorder_iter(root);
-	printf("\n\n");
-	
+	char input[100];
+	while (1) {
+		printf("=========메뉴=========\n");
+		printf("s) 검색\n");
+		printf("i) 노드 추가\n");
+		printf("d) 노드 삭제\n");
+		printf("t) 중위 순회\n");
+		printf("l) 노드 추가(반복)\n");
+		printf("D) 노드삭제(반복)\n");
+		printf("c) 종료\n");
+		printf("메뉴를 선택하세요: ");
+		scanf_s("%s", input, sizeof(input));
+		if (strlen(input) == 1) {
+			char c = input[0];
+			switch (c) {
+			case 's':
+				printf("검색할 값 입력: ");
+				scanf_s("%d", &k);
+				if (search(root, 30) != NULL)
+					printf("검색 성공: %d\n", k);
+				else
+					printf("%d는 트리에 없습니다.\n",k);
+				break;
+			case 'i':
+				
+				break;
+			case 'd':
+				
+				break;
+			case 't':
+				
+				break;
+			case 'l':
+				
+				break;
+			case 'D':
+
+				break;
+			case 'c':
+				printf("프로그램을 종료합니다.\n");
+				exit(0);
+			default:
+				printf("다시 선택해주세요.\n");
+				while (getchar() != '\n');
+			}
+		}
+		else {
+			printf("다시 선택해주세요.\n");
+
+		}
+
+	}
 	return 0;
 }
