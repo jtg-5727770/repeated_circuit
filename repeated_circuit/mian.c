6fd5f95a32f54492a0dd4f2a647fa6fb3df31ea4
5727770@stu.kmu.ac.kr <5727770@stu.kmu.ac.kr>
2023-10-17 10:58:30 +0900

    1017_실습

--- repeated_circuit/mian.c
+++ repeated_circuit/mian.c
@@ -1,91 +1,276 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <memory.h>
+#define MAX_ELEMENT 200
 
+//히프 정렬 프로그램
 
-typedef struct TreeNode {
-	int data;
-	struct TreeNode* left, * right;
-} TreeNode;
+typedef struct {
+	int key;
+} element;
+typedef struct {
+	element heap[MAX_ELEMENT];
+	int heap_size;
+} HeapType;
 
-#define SIZE 100
-int top = -1;
-TreeNode* stack[SIZE];
-
-// push 함수
-void push(TreeNode* p)
+// 생성 함수
+HeapType* create()
 {
-	if (top < SIZE - 1)
-		stack[++top] = p;
+	return (HeapType*)malloc(sizeof(HeapType));
 }
-// pop 함수
-TreeNode* pop()
+// 초기화 함수
+void init(HeapType* h)
 {
-	TreeNode* p = NULL;
-	if (top >= 0)
-		p = stack[top--];
-	return p;
+	h->heap_size = 0;
 }
-// 전위 순회
-void preorder_iter(TreeNode* root)
+// 현재 요소의 개수가 heap_size인 히프 h에 item을 삽입한다.
+// 삽입 함수
+void insert_max_heap(HeapType* h, element item)
 {
-	while (1) {
-		for (; root; root = root->left) {
-			push(root);
-			printf("%d ", root->data);
-		}
-		root = pop();
-		if (!root) break;
-		root = root->right;
+	int i;
+	i = ++(h->heap_size);
+
+	//  트리를 거슬러 올라가면서 부모 노드와 비교하는 과정
+	while ((i != 1) && (item.key > h->heap[i / 2].key)) {
+		h->heap[i] = h->heap[i / 2];
+		i /= 2;
 	}
-}
-// 중위 순회
-void inorder_iter(TreeNode* root)
-{
-	while (1) {
-		for (; root; root = root->left)
-			push(root);
-		root = pop();
-		if (!root) break;
-		printf("%d ", root->data);
-		root = root->right;
+	h->heap[i] = item;     // 새로운 노드를 삽입
+	// 노드가 이동될때마다 힙 상태 출력
+	for (int j = 1; j <= h->heap_size; j++) {
+		printf("%d ", h->heap[j].key);
 	}
+	printf("\n");
 }
-// 후위 순회
-void postorder_iter(TreeNode* root)
+// 삭제 함수
+element delete_max_heap(HeapType* h)
 {
+	int parent, child;
+	element item, temp;
 
+	item = h->heap[1];
+	temp = h->heap[(h->heap_size)--];
+	parent = 1;
+	child = 2;
+	while (child <= h->heap_size) {
+		// 현재 노드의 자식노드 중 더 작은 자식노드를 찾는다.
+		if ((child < h->heap_size) &&
+			(h->heap[child].key) < h->heap[child + 1].key)
+			child++;
+		if (temp.key >= h->heap[child].key) break;
+		// 한 단계 아래로 이동
+		h->heap[parent] = h->heap[child];
+		parent = child;
+		child *= 2;
+	}
+	h->heap[parent] = temp;
+
+	// 노드가 이동될때마다 힙 상태 출력
+	for (int j = 1; j <= h->heap_size; j++) {
+		printf("%d ", h->heap[j].key);
+	}
+	printf("\n");
+
+	return item;
 }
-
-//링크 노드 값입력
-TreeNode n1 = { 4, NULL, NULL };
-TreeNode n2 = { 5, NULL, NULL };
-TreeNode n3 = { 3, &n1, &n2 };
-TreeNode n4 = { 6, NULL, NULL };
-TreeNode n5 = { 2, &n3, &n4 };
-TreeNode n6 = { 10, NULL, NULL };
-TreeNode n7 = { 11, NULL, NULL };
-TreeNode n8 = { 9, &n6, &n7 };
-TreeNode n9 = { 8, NULL, NULL };
-TreeNode n10 = { 7, &n9, &n8 };
-TreeNode n11 = { 1, &n5, &n10 };
-TreeNode* root = &n11;
-
-void  main()
+// 우선 순위 큐인 히프를 이용한 정렬
+void heap_sort(element a[], int n)
 {
-	printf("[Linked Tree]\n");
-	
-	printf("1.전위 순회\n");
-	preorder_iter(root);
-	printf("\n\n");
-	
-	printf("2.중위 순회\n");
-	inorder_iter(root);
-	printf("\n\n");
+	int i;
+	HeapType* h;
 
-	printf("3.후위 순회\n");
-	postorder_iter(root);
-	printf("\n\n");
+	h = create();
+	init(h);
+	for (i = 0; i < n; i++) {
+		insert_max_heap(h, a[i]);
+	}
+	for (i = (n - 1); i >= 0; i--) {
+		a[i] = delete_max_heap(h);
+	}
+	free(h);
+}
+#define SIZE 17
+int main(void)
+{
+	element list[SIZE] = { 34, 12, 76, 59, 32, 55, 88, 26, 16, 79, 34, 85, 29, 78, 41, 56, 86 };
 	
+	HeapType* heap;
+
+	heap = create(); 	// 히프 생성
+	init(heap);
+
+	delete_max_heap(heap);
+
+	heap_sort(list, SIZE);
+	printf("\n");
+	for (int i = 0; i < SIZE; i++) {
+		printf("%d ", list[i].key);
+	}
+	printf("\n");
 	return 0;
 }
+
+
+//히프만 코드 생성
+/*
+typedef struct TreeNode {
+	int weight;
+	char ch;
+	struct TreeNode* left;
+	struct TreeNode* right;
+} TreeNode;
+
+typedef struct Element {
+	TreeNode* ptree;
+	char ch;
+	int key;
+} element;
+
+Typedef struct  heapType {
+	element heap[MAX_ELEMENT];
+	int heap_size;
+} HeapType;
+
+// 생성 함수
+HeapType* create()
+{
+	return (HeapType*)malloc(sizeof(HeapType));
+}
+// 초기화 함수
+void init(HeapType* h)
+{
+	h->heap_size = 0;
+}
+// 현재 요소의 개수가 heap_size인 히프 h에 item을 삽입한다.
+// 삽입 함수
+void insert_min_heap(HeapType* h, element item)
+{
+	int i;
+	i = ++(h->heap_size);
+
+	//  트리를 거슬러 올라가면서 부모 노드와 비교하는 과정
+	while ((i != 1) && (item.key < h->heap[i / 2].key)) {
+		h->heap[i] = h->heap[i / 2];
+		i /= 2;
+	}
+	h->heap[i] = item;     // 새로운 노드를 삽입
+}
+// 삭제 함수
+element delete_min_heap(HeapType* h)
+{
+	int parent, child;
+	element item, temp;
+
+	item = h->heap[1];
+	temp = h->heap[(h->heap_size)--];
+	parent = 1;
+	child = 2;
+	while (child <= h->heap_size) {
+		// 현재 노드의 자식노드중 더 작은 자식노드를 찾는다.
+		if ((child < h->heap_size) &&
+			(h->heap[child].key) > h->heap[child + 1].key)
+			child++;
+		if (temp.key < h->heap[child].key) break;
+		// 한 단계 아래로 이동
+		h->heap[parent] = h->heap[child];
+		parent = child;
+		child *= 2;
+	}
+	h->heap[parent] = temp;
+	return item;
+}
+// 이진 트리 생성 함수
+TreeNode* make_tree(TreeNode* left,
+	TreeNode* right)
+{
+	TreeNode* node =
+		(TreeNode*)malloc(sizeof(TreeNode));
+	node->left = left;
+	node->right = right;
+	return node;
+}
+// 이진 트리 제거 함수
+void destroy_tree(TreeNode* root)
+{
+	if (root == NULL) return;
+	destroy_tree(root->left);
+	destroy_tree(root->right);
+	free(root);
+}
+
+int is_leaf(TreeNode* root)
+{
+	return !(root->left) && !(root->right);
+}
+void print_array(int codes[], int n)
+{
+	for (int i = 0; i < n; i++)
+		printf("%d", codes[i]);
+	printf("\n");
+}
+
+void print_codes(TreeNode* root, int codes[], int top)
+{
+
+	// 1을 저장하고 순환호출한다. 
+	if (root->left) {
+		codes[top] = 1;
+		print_codes(root->left, codes, top + 1);
+	}
+
+	// 0을 저장하고 순환호출한다. 
+	if (root->right) {
+		codes[top] = 0;
+		print_codes(root->right, codes, top + 1);
+	}
+
+	// 단말노드이면 코드를 출력한다. 
+	if (is_leaf(root)) {
+		printf("%c: ", root->ch);
+		print_array(codes, top);
+	}
+}
+// 허프만 코드 생성 함수
+void huffman_tree(int freq[], char ch_list[], int n)
+{
+	int i;
+	TreeNode* node, * x;
+	HeapType* heap;
+	element e, e1, e2;
+	int codes[100];
+	int top = 0;
+
+	heap = create();
+	init(heap);
+	for (i = 0; i < n; i++) {
+		node = make_tree(NULL, NULL);
+		e.ch = node->ch = ch_list[i];
+		e.key = node->weight = freq[i];
+		e.ptree = node;
+		insert_min_heap(heap, e);
+	}
+	for (i = 1; i < n; i++) {
+		// 최소값을 가지는 두개의 노드를 삭제
+		e1 = delete_min_heap(heap);
+		e2 = delete_min_heap(heap);
+		// 두개의 노드를 합친다.
+		x = make_tree(e1.ptree, e2.ptree);
+		e.key = x->weight = e1.key + e2.key;
+		e.ptree = x;
+		printf("%d+%d->%d \n", e1.key, e2.key, e.key);
+		insert_min_heap(heap, e);
+	}
+	e = delete_min_heap(heap); // 최종 트리
+	print_codes(e.ptree, codes, top);
+	destroy_tree(e.ptree);
+	free(heap);
+}
+
+int main(void)
+{
+
+	char ch_list[] = { 'a', 'e', 'i', 'o', 'u', 's', 't'};
+	int freq[] = { 10, 15, 12, 3, 4, 13, 1 };
+	huffman_tree(freq, ch_list, 7);
+	return 0;
+}
+*/
