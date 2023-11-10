ace20cf5a2f2b4b6b2e8c9ee52010eadbc16a4a9
5727770@stu.kmu.ac.kr <5727770@stu.kmu.ac.kr>
2023-11-10 22:41:44 +0900

    코드 정리

--- repeated_circuit/mian.c
+++ repeated_circuit/mian.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
@@ -24,20 +23,50 @@ typedef struct GraphType {
 	int weight[MAX_VERTICES][MAX_VERTICES];
 } Mat_GraphType;
 
+// 간선을 나타내는 구조체
+typedef struct EdgeNode {
+	int dest; // 목적지 정점
+	int weight; // 가중치
+	struct EdgeNode* next; // 다음 간선을 가리키는 포인터
+} EdgeNode;
+
+// 인접 리스트 방식 그래프
+typedef struct GraphType1 {
+	int n; // 정점의 개수
+	EdgeNode* adj_list[MAX_VERTICES]; // 각 정점의 인접 리스트를 나타내는 배열
+} List_GraphType;
+
 // 탐색하며 min값 확인
-int choose(int distance[], int n, int found[])
-{
+int mat_choose(int distance[], int n, int found[]) {
 	int i, min, minpos;
-	min = INT_MAX;
+	min = INT_MAX;    // 현재까지의 최소 거리를 무한대로 초기화
 	minpos = -1;
+
+	// 방문하지 않은 정점 중에서 최소 거리를 가진 정점을 찾음
 	for (i = 0; i < n; i++)
 		if (distance[i] < min && !found[i]) {
 			min = distance[i];
 			minpos = i;
 		}
-	mat[mat_j] = minpos + 1;
+
+	mat[mat_j] = minpos + 1;  // 최단 거리를 가진 정점의 인덱스를 배열에 저장
 	mat_j++;
-	return minpos; // 최단 거리를 가진 정점의 인덱스 반환
+
+	return minpos;  // 최단 거리를 가진 정점의 인덱스 반환
+}
+int list_choose(int distance[], int n, int found[]) {
+	int i, min, minpos;
+	min = INT_MAX;    // 현재까지의 최소 거리를 무한대로 초기화
+	minpos = -1;
+
+	// 방문하지 않은 정점 중에서 최소 거리를 가진 정점을 찾음
+	for (i = 0; i < n; i++)
+		if (distance[i] < min && !found[i]) {
+			min = distance[i];
+			minpos = i;
+		}
+
+	return minpos;  // 최단 거리를 가진 정점의 인덱스 반환
 }
 
 void print_status(Mat_GraphType* g) {
@@ -72,7 +101,7 @@ void shortest_path(Mat_GraphType* g, int start)
 	mat_j++;
 	for (i = 0; i < g->n - 1; i++) {
 		print_status(g);
-		u = choose(distance, g->n, found); // min값 확인
+		u = mat_choose(distance, g->n, found); // min값 확인
 		
 		found[u] = TRUE;  // 최소거리 정점 방문표시
 		for (w = 0; w < g->n; w++) {  //최소거리 업데이트를 하기위한 반복
@@ -87,49 +116,36 @@ void shortest_path(Mat_GraphType* g, int start)
 		printf("%d ", mat[j]);
 	
 }
-// 간선을 나타내는 구조체
-typedef struct EdgeNode {
-	int dest; // 목적지 정점
-	int weight; // 가중치
-	struct EdgeNode* next; // 다음 간선을 가리키는 포인터
-} EdgeNode;
 
-// 인접 리스트 방식 그래프
-typedef struct GraphType1 {
-	int n; // 정점의 개수
-	EdgeNode* adj_list[MAX_VERTICES]; // 각 정점의 인접 리스트를 나타내는 배열
-} List_GraphType;
-
+// 인접 리스트 방식 그래프에서 시작 정점부터의 최단 경로를 찾는 함수
 void list_shortest_path(List_GraphType* g, int start) {
-	printf("인접 리스트 방식\n");
+	printf("인접 리스트 방식\n");  // 현재 사용 중인 방식을 출력
 	int i, u, w;
 	for (i = 0; i < g->n; i++) {
-		distance[i] = INF;
-		found[i] = FALSE;
+		distance[i] = INF;  // 시작 정점으로부터의 거리를 무한대로 초기화
+		found[i] = FALSE;   // 아직 방문하지 않음을 표시
 	}
-	distance[start] = 0;
-
+	distance[start] = 0;    // 시작 정점의 거리는 0으로 설정
 
 	for (i = 0; i < g->n; i++) {
+		u = list_choose(distance, g->n, found);  // 아직 방문하지 않은 정점 중 최단 거리를 가진 정점 선택
 
-		u = choose(distance, g->n, found);
-
-		EdgeNode* current = g->adj_list[u];
+		EdgeNode* current = g->adj_list[u];  // 선택한 정점에 대한 인접 리스트의 첫 번째 노드를 얻음
 		while (current != NULL) {
-			w = current->dest;
+			w = current->dest;  // 현재 간선의 목적지 정점
 			if (!found[w] && (distance[u] + current->weight < distance[w])) {
-				distance[w] = distance[u] + current->weight;
+				distance[w] = distance[u] + current->weight;  // 최단 경로 업데이트
 			}
-			current = current->next;
+			current = current->next;  // 다음 인접 정점으로 이동
 		}
-		found[u] = TRUE;
-		list[list_j] = u + 1;
+		found[u] = TRUE;  // 최소 거리를 가진 정점을 방문했음을 표시
+		list[list_j] = u + 1;  // 최소 거리를 가진 정점을 리스트에 추가
 		list_j++;
-		print_status(g);
+		print_status(g);  // 현재까지의 최단 거리 상태를 출력
 	}
 
 	for (int j = 0; j < g->n; j++)
-		printf("%d ", list[j]);
+		printf("%d ", list[j]); // 최종적으로 찾은 최단 경로를 출력
 }
 
 // 그래프 초기화 함수
@@ -166,11 +182,13 @@ int main(void)
 	{ INF, INF, INF, INF, 18, INF, INF,INF, 0, 10},
 	{ INF, INF, INF, 16, INF, INF, INF, 15, 10, 0}} };
 	shortest_path(&g, 0);
-	printf("\n");
+	
+	printf("\n\n");
+	
 	List_GraphType g1;
 	init_graph(&g1, 10);
 
-	// 간선 추가
+	// 인접 리스트 방식 간선 추가
 	add_edge(&g1, 0, 1, 3);
 	add_edge(&g1, 0, 5, 11);
 	add_edge(&g1, 0, 6, 12);
@@ -196,5 +214,3 @@ int main(void)
 	
 	return 0;
 }
-
-
