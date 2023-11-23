fccae42f263e9bb4b949520dcc76455309bdc591
5727770@stu.kmu.ac.kr <5727770@stu.kmu.ac.kr>
2023-11-23 10:46:51 +0900

    1123_실습

--- repeated_circuit/mian.c
+++ repeated_circuit/mian.c
@@ -1,91 +1,74 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <memory.h>
 
 
-typedef struct TreeNode {
-	int data;
-	struct TreeNode* left, * right;
-} TreeNode;
+int move_count = 0;     //이동횟수 카운트
+int compare_count = 0;  //비교횟수 카운트
+int n1;
 
-#define SIZE 100
-int top = -1;
-TreeNode* stack[SIZE];
-
-// push 함수
-void push(TreeNode* p)
+// gap 만큼 떨어진 요소들을 삽입 정렬
+// 정렬의 범위는 first에서 last
+inc_insertion_sort(int list[], int first, int last, int gap)
 {
-	if (top < SIZE - 1)
-		stack[++top] = p;
-}
-// pop 함수
-TreeNode* pop()
-{
-	TreeNode* p = NULL;
-	if (top >= 0)
-		p = stack[top--];
-	return p;
-}
-// 전위 순회
-void preorder_iter(TreeNode* root)
-{
-	while (1) {
-		for (; root; root = root->left) {
-			push(root);
-			printf("%d ", root->data);
+	int a, i, j, key;
+	for (i = first + gap; i <= last; i = i + gap) {
+
+		key =  a = list[i];
+		for (j = i - gap; j > first && key < list[j]; j = j - gap) {
+			list[j + gap] = list[j];
+			move_count += 1;
+			
 		}
-		root = pop();
-		if (!root) break;
-		root = root->right;
+		list[j + gap] = key;	
+		if (a != list[i])
+			compare_count += 1;
 	}
 }
-// 중위 순회
-void inorder_iter(TreeNode* root)
+// 쉘정렬
+void shell_sort(int list[], int n)   // n = size
 {
-	while (1) {
-		for (; root; root = root->left)
-			push(root);
-		root = pop();
-		if (!root) break;
-		printf("%d ", root->data);
-		root = root->right;
+	
+	int i, gap;
+	for (gap = n / 2; gap > 0; gap = gap / 2) {
+		if ((gap % 2) == 0) gap++;
+		for (i = 0; i < gap; i++) {		// 부분 리스트의 개수는 gap
+			inc_insertion_sort(list, i, n - 1, gap);
+
+		}
+		if (n1 == 0) {
+			
+			for (int j = 0; j < 20; j++) {
+				printf("%d ", list[j]);
+			}
+			printf("\n");
+		}
 	}
 }
-// 후위 순회
-void postorder_iter(TreeNode* root)
-{
 
-}
-
-//링크 노드 값입력
-TreeNode n1 = { 4, NULL, NULL };
-TreeNode n2 = { 5, NULL, NULL };
-TreeNode n3 = { 3, &n1, &n2 };
-TreeNode n4 = { 6, NULL, NULL };
-TreeNode n5 = { 2, &n3, &n4 };
-TreeNode n6 = { 10, NULL, NULL };
-TreeNode n7 = { 11, NULL, NULL };
-TreeNode n8 = { 9, &n6, &n7 };
-TreeNode n9 = { 8, NULL, NULL };
-TreeNode n10 = { 7, &n9, &n8 };
-TreeNode n11 = { 1, &n5, &n10 };
-TreeNode* root = &n11;
 
 void  main()
 {
-	printf("[Linked Tree]\n");
+	for (n1 = 0; n1 < 20; n1++) {
+		srand(time(NULL)); // 난수 초기화
+		int list_O[20];
+		int random = 0; // 정수형 변수 선언
+		for (int i = 0; i < 20; i++) {
+			random = rand() % 100; // 난수 생성
+			list_O[i] = random;
+		}
+		if (n1 == 0) {
+			printf("초기 리스트\n");
+			for (int i = 0; i < 20; i++) {
+				printf("%d ", list_O[i]);
+			}
+			printf("\n\n쉘 정렬\n");
+		}
 	
-	printf("1.전위 순회\n");
-	preorder_iter(root);
-	printf("\n\n");
-	
-	printf("2.중위 순회\n");
-	inorder_iter(root);
-	printf("\n\n");
+		shell_sort(list_O, 20);
+	}
 
-	printf("3.후위 순회\n");
-	postorder_iter(root);
-	printf("\n\n");
-	
+	printf("\n이동 횟수 평균: %d\n", move_count / 20);
+	printf("비교 횟수 평균: %d", compare_count / 20);
+
 	return 0;
 }
